393dc53e5403342719b4ef6de5c90db69133dc46
xs.Liu <liuxianshi@baozhunniu.com>
2021-07-29 17:50:22 +0800

    增加BaseMapper

---
+++ src/main/java/com/mango/core/servlet/BaseMapper.java
@@ -0,0 +1,11 @@
+package com.mango.core.servlet;
+
+/**
+ * 通用mapper
+ *
+ * @author xs.Liu
+ * @version 1.0.0
+ * @since 2021/7/29 17:48
+ */
+public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
+}
